f02c5a0a6dd64e8d5d38e69af4ce57fd9fae7e1d
SIRI <sireeshasiri946@gmail.com>
2024-11-03 17:45:08 +0530

    adding meta file

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "instance" {
     command = <<EOL
 cd /home/ec2-user/roboshop-ansible
 ansible-playbook -i ${self.private_ip}, -e ansible_user=ec2-user -e ansible_password=DevOps321
- main.yml
+ roboshop.yml
 EOL
   }
 }
